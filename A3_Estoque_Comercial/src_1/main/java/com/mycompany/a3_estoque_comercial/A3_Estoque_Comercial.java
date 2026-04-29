41fd42361bdd89adf6fbce7c6cc7b571ff6616b4
mateuspaulistk <mateuspaulistahnke@gmail.com>
2026-04-29 17:00:16 -0300

    ola mundo

---
+++ A3_Estoque_Comercial/src_1/main/java/com/mycompany/a3_estoque_comercial/A3_Estoque_Comercial.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.a3_estoque_comercial;
+
+public class A3_Estoque_Comercial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
